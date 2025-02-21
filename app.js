2bf34726dd9b521e363e8a3588ec9b1915d57846
nxz10 <NValdes00@hotmail.com>
2025-02-21 20:44:10 -0300

    Se crean las funciones de Agregar amigos, imprimir lista y limpiar caja de texto

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let participantes = [];
+
+function agregarAmigo() {
+    let nombreIngresado = document.getElementById('amigo').value;
+
+    if (nombreIngresado != '') {
+        participantes.push(nombreIngresado);
+        console.log(participantes);
+        // console.log(typeof(participantes));
+    } else {
+        alert("Por favor, inserte un nombre. (Error campo vacío)");
+    }
+
+    limpiarInputUsuario();
+    imprimirLista();
+
+    return;
+}
+
+function imprimirLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    for (let i = 0; i < participantes.length; i++) {
+        let elementoLi = document.createElement("li");
+        elementoLi.textContent = participantes[i];
+        lista.appendChild(elementoLi);
+        // console.log(participantes[i]);
+    }
+}
+
+function limpiarInputUsuario() {
+    let valorInputUsuario = document.getElementById('amigo');
+    valorInputUsuario.value = '';
+}
